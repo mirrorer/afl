5a263185139232257b28c23ed6ddfa86b7840d05
Yuan Kang <yuanjk@cs.columbia.edu>
2017-07-26 12:35:55 -0400

    2.47b

--- config.h
+++ config.h
@@ -21,7 +21,7 @@
 
 /* Version string: */
 
-#define VERSION             "2.46b"
+#define VERSION             "2.47b"
 
 /******************************************************
  *                                                    *
