a4a97670eb2697241eb977d55645e49e837852d9
Thomas HUET <git@thomash.fr>
2017-07-29 16:26:10 +0200

    2.49b

--- config.h
+++ config.h
@@ -21,7 +21,7 @@
 
 /* Version string: */
 
-#define VERSION             "2.48b"
+#define VERSION             "2.49b"
 
 /******************************************************
  *                                                    *
